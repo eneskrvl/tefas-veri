d1976286c41d4136894483a390db9208432c77e3
eneskrvl <eneskirveli@gmail.com>
2025-08-02 18:20:25 +0300

    tefas_scraper.py

--- tefas_scraper.py
+++ tefas_scraper.py
@@ -4,13 +4,12 @@ import pandas as pd
 
 async def get_hkh_data():
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=True)  # Headless TRUE şart GitHub için
+        browser = await p.chromium.launch(headless=True)
         page = await browser.new_page()
         await page.goto("https://www.tefas.gov.tr/FonAnaliz.aspx?FonKod=HKH")
 
         try:
-            await page.wait_for_selector("#MainContent_lblTarih", timeout=60000)
-
+            await page.wait_for_selector("#MainContent_lblBirimPayDegeri", timeout=60000)
             tarih = await page.inner_text("#MainContent_lblTarih")
             fiyat = await page.inner_text("#MainContent_lblBirimPayDegeri")
 
@@ -23,10 +22,10 @@ async def get_hkh_data():
 
             df = pd.DataFrame(data)
             df.to_csv("tefas_gunluk.csv", index=False)
-            print("✅ TEFAS verisi başarıyla kaydedildi.")
+            print("✅ CSV yazıldı:", df)
 
         except Exception as e:
-            print(f"❌ Hata oluştu: {e}")
+            print("❌ Hata:", e)
         finally:
             await browser.close()
 
